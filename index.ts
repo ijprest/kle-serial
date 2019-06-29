fcd05fa9a6936ae0dffe08c8373b8bc13ba689e8
Ian Prest <ijprest@users.noreply.github.com>
2019-06-29 16:28:13 -0700

    initial implementation

--- index.ts
+++ index.ts
@@ -1,3 +1,74 @@
-export function foo() {
-    
+import * as JSON5 from 'json5';
+import { keys } from 'ts-transformer-keys';
+
+export class Key {
+  color: string = "#cccccc";
+  labels: string[] = [];
+  textColor: string[] = [];
+  textSize: number[] = [];
+  default: { textColor: string, textSize: number } = { textColor: "#000000", textSize: 3 };
+  x: number = 0;
+  y: number = 0;
+  width: number = 1;
+  height: number = 1;
+  x2: number = 0;
+  y2: number = 0;
+  width2: number = 1;
+  height2: number = 1;
+  rotation_x: number = 0;
+  rotation_y: number = 0;
+  rotation_angle: number = 0;
+  decal: boolean = false;
+  ghost: boolean = false;
+  stepped: boolean = false;
+  nub: false = false;
+  profile: string = "";
+  sm: string = ""; // switch mount
+  sb: string = ""; // switch brand
+  st: string = ""; // switch type
+}
+
+export interface KeyboardMetadata {
+  author?: string;
+  backcolor?: string;
+  background?: string;
+  name?: string;
+  notes?: string;
+  radii?: string;
+  switchBrand?: string;
+  switchMount?: string;
+  switchType?: string;
+}
+const KeyboardMetadataProps = keys<KeyboardMetadata>();
+
+export interface Keyboard {
+  meta: KeyboardMetadata;
+  keys: Key[];
+}
+
+export function deserialize(rows: Array<any>): Keyboard | null {
+  if (!(rows instanceof Array))
+    return null;
+
+  let meta: KeyboardMetadata = {};
+  let keys: Key[] = [];
+
+  for (var r = 0; r < rows.length; ++r) {
+    if (typeof rows[r] === 'object') {
+      if (r != 0) {
+        throw "Error: keyboard metadata must the be first element:\n  " + JSON5.stringify(rows[r]);
+      }
+      for (let prop of KeyboardMetadataProps) {
+        if (rows[r][prop]) {
+          meta[prop] = rows[r][prop];
+        }
+      }
+    }
+  }
+
+  return { meta: meta, keys: keys };
+}
+
+export function parse(json: string) {
+  //return deserialize(json5.parse(json));
 }
--- test/test.js
+++ test/test.js
@@ -1,10 +1,34 @@
 'use strict';
 
 var expect = require('chai').expect;
-var serial = require('../dist/index');
+var kbd = require('../dist/index');
 
-describe("serial", function () {
-  it('should do nothing', function () {
-    serial.foo();
+describe("serialization", function () {
+
+  it('should return null on non-array', function () {
+    var result = kbd.deserialize("test");
+    expect(result).to.be.null;
+  });
+
+  it('should return empty keyboard on empty array', function () {
+    var result = kbd.deserialize([]);
+    expect(result).to.be.an("object");
+    expect(result).to.have.property("keys");
+    expect(result.keys).to.be.an("array");
+    expect(result.keys).to.be.empty;
+  });
+
+  it('should parse metadata from first object if it exists', function () {
+    var result = kbd.deserialize([{ name: "test" }]);
+    expect(result).to.be.an("object");
+    expect(result).to.have.property("meta");
+    expect(result.meta).to.have.property("name");
+    expect(result.meta.name).to.equal("test");
+    console.log(result);
+  });
+
+  it('should throw an exception if metadata is anywhere other than the start', function () {
+    var result = () => kbd.deserialize([[], { name: "test" }]);
+    expect(result).to.throw();
   });
 });
