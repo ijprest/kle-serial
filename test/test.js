a48d2808184a932c19a13ca514be07665f128066
Ian Prest <ijprest@users.noreply.github.com>
2019-06-29 14:56:31 -0700

    added stub test file

---
+++ test/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var expect = require('chai').expect;
+var serial = require('../dist/index');
+
+describe("serial", function () {
+  it('should do nothing', function () {
+    serial.foo();
+  });
+});
