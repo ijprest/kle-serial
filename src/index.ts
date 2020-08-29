14d30d622af3207df0484fc269655cd7f7389bc4
Buckwich <s.steinruecken@gmx.net>
2020-08-29 21:22:05 +0200

    add missing exports

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,18 @@ import * as JSON5 from "json5";
 import { deserialize } from "./deserialize";
 import { Keyboard } from "./interfaces";
 import { serialize } from "./serialize";
-export module Serial {
-  deserialize;
-  serialize;
 
-  export function parse(json: string): Keyboard {
-    return deserialize(JSON5.parse(json));
-  }
-  export function stringify(keyboard: Keyboard): string {
-    return JSON.stringify(serialize(keyboard));
-  }
+export { deserialize } from "./deserialize";
+export {
+  Key as KleKey,
+  Keyboard as KleKeyboard,
+  KeyboardMetadata as KleKeyboardMetadata
+} from "./interfaces";
+export { serialize } from "./serialize";
+
+export function parse(json: string): Keyboard {
+  return deserialize(JSON5.parse(json));
+}
+export function stringify(keyboard: Keyboard): string {
+  return JSON.stringify(serialize(keyboard));
 }
